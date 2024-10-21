8b5395ef04f8ca81f34c07976c36b6c1db353c5c
Luji Ramdhan <lrdn03@gmail.com>
2024-10-21 19:54:05 +0700

    feat: Finishing invalid state

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -2,7 +2,7 @@ import { memo } from "react";
 
 const SubmitButton = () => {
   return (<div>
-    <button className="bg-primary-green-medium text-neutral-white w-full font-semibold capitalize text-lg rounded-md py-3 text-[16px]">submit</button>
+    <button type="submit" className="bg-primary-green-medium text-neutral-white w-full font-semibold capitalize text-lg rounded-md py-3 text-[16px]">submit</button>
   </div>);
 };
 
